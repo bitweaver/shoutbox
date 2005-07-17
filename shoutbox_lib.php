1e0b49213da91678afb7294515297b34afa5ce90
Max Kremmel <xing@synapse.plus.com>
2005-07-17 17:36:16 +0000

    Merge recent changes from R1 to HEAD - never doing this again.

--- shoutbox_lib.php
+++ shoutbox_lib.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_shoutbox/Attic/shoutbox_lib.php,v 1.2 2005/06/28 07:45:57 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_shoutbox/Attic/shoutbox_lib.php,v 1.3 2005/07/17 17:36:15 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: shoutbox_lib.php,v 1.2 2005/06/28 07:45:57 spiderr Exp $
+ * $Id: shoutbox_lib.php,v 1.3 2005/07/17 17:36:15 squareing Exp $
  * @package shoutbox
  */
 
@@ -31,18 +31,18 @@ class ShoutboxLib extends BitBase {
 		$bindvars = array();
 		$mid = '';
 		if( !empty( $pListHash['find'] ) ) {
-			$mid = " where (UPPER(`shout_message`) like ?)";
+			$mid = " WHERE (UPPER(`shout_message`) like ?)";
 			$bindvars = array('%'.strtoupper( $pListHash['find'] ).'%');
 		}
 
 		if( !empty( $pListHash['user_id'] ) ) {
-			$mid = empty( $mid ) ? ' WHERE ' : ' AND ';
+			$mid .= empty( $mid ) ? ' WHERE ' : ' AND ';
 			$mid .= " `shout_user_id` = ?";
 			array_push( $bindvars, $pListHash['user_id'] );
 		}
 
 		if( !empty( $pListHash['to_user_id'] ) ) {
-			$mid = empty( $mid ) ? ' WHERE ' : ' AND ';
+			$mid .= empty( $mid ) ? ' WHERE ' : ' AND ';
 			$mid .= " `to_user_id` = ?";
 			array_push( $bindvars, $pListHash['to_user_id'] );
 		}
